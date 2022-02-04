be905beb4eb31cfccbe9a02312c7c71b19cba62c
adylansd28 <adylansd28@gmail.com>
2022-02-04 17:17:29 -0500

    W05 Chamber Update

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -1,15 +1,30 @@
+/* Formatted Date Header */
 const datefield = document.querySelector("date");
 const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
 datefield.innerHTML = `<em>${fulldate}</em>`;
 
+
+/* Hamburger Button */
 function toggleMenu () {
     document.getElementById("primary-nav").classList.toggle("open");
     document.getElementById("hamburger-btn").classList.toggle("open");
 }
+
 const x = document.getElementById("hamburger-btn");
 x.onclick = toggleMenu;
 
 const date = new Date(document.lastModified).toLocaleString();
 
 document.querySelector("#last-updated").innerHTML = date;
+
+
+/* Monday and Tuesday Message */
+const dayNumber =now.getDay() - 4;
+const message = document.getElementById("message");
+
+if ((dayNumber == 1) || (dayNumber == 2)) {
+    message.classList.add("showme")
+} else {
+    message.classList.add("hideme")
+}
